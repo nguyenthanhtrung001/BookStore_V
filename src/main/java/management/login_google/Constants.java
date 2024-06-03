327261ab13e73cc11ac8f4258832643a87ea5074
nguyenthanhtrung001 <thanhtrung8674@gmail.com>
2024-06-03 14:32:34 +0700

    update-login-google

---
+++ src/main/java/management/login_google/Constants.java
@@ -0,0 +1,17 @@
+package management.login_google;
+
+public class Constants {
+	public static String GOOGLE_CLIENT_ID = "242828357380-bmi9embekgk0pqkcl14jqt35g80letup.apps.googleusercontent.com";
+
+	public static String GOOGLE_CLIENT_SECRET = "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";
+
+	public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/login_google/LoginGoogleHandler";
+
+	public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
+
+	public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
+
+	public static String GOOGLE_GRANT_TYPE = "authorization_code";
+	//
+	//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
+}
